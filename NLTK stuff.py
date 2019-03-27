cdb43393ef21e2d22728b665ee70f9b9de354405
mel1 <mel1@vt.edu>
2019-03-27 13:57:00 -0400

    added stemming functionality

--- NLTK stuff.py
+++ NLTK stuff.py
@@ -1,21 +1,25 @@
 import nltk
+from nltk.stem.snowball import EnglishStemmer
+stemmer = EnglishStemmer()
 
 def train():
 	pass
 
 def sentimentCheck(word):
     print("Token: " + word)
+    print("Stemmed: " + stemmer.stem(word) + "\n")
 
 
 def test():
     sentence = str(input())
     tokens = nltk.word_tokenize(sentence)
 
+
     print("\nList of Tokens: ")
     print(tokens)
     print()
 
     for token in tokens:
-        sentimentCheck(token)
+       sentimentCheck(token)
 
 test()
