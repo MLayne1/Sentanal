db1cf6985fa8e0c3e2589cd67d768e566ceac9c8
mel1 <mel1@vt.edu>
2019-03-27 15:22:48 -0400

    added file reading and a training data file

--- NLTK stuff.py
+++ NLTK stuff.py
@@ -3,7 +3,13 @@ from nltk.stem.snowball import EnglishStemmer
 stemmer = EnglishStemmer()
 
 def train():
-	pass
+	trainingData = open("trainingData.txt", "r")
+
+	for line in trainingData:
+		values = line.split(", ")
+		for value in values:
+			value.strip("\n")
+			print(value)
 
 def sentimentCheck(word):
     print("Token: " + word)
@@ -20,3 +26,7 @@ def test():
        sentimentCheck(token)
 
 test()
+
+print("\n\n\n")
+
+train()
