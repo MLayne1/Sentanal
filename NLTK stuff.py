ea211c2df34a9835b8954ad5954af016d2eb91d3
mel1 <mel1@vt.edu>
2019-03-27 12:14:19 -0400

    added functionality to basi script

--- NLTK stuff.py
+++ NLTK stuff.py
@@ -4,12 +4,16 @@ def train():
 	pass
 
 def sentimentCheck(word):
-    print(word)
+    print("Token: " + word)
 
 
 def test():
     sentence = str(input())
     tokens = nltk.word_tokenize(sentence)
+
+    print("\nList of Tokens: ")
+    print(tokens)
+    print()
 
     for token in tokens:
         sentimentCheck(token)
