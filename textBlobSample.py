a29cc82618bdea36742cffb3d8e69e0225154974
Luis Hernandez <ziul.ch@gmail.com>
2019-03-27 15:38:28 -0400

    Updated TextBlob Files

--- textBlobSample.py
+++ textBlobSample.py
@@ -1,26 +1,5 @@
 from textblob.classifiers import NaiveBayesClassifier
 
-train = [
-    ('I love this sandwich.', 'pos'),
-    ('this is an amazing place!', 'pos'),
-    ('I feel very good about these beers.', 'pos'),
-    ('this is my best work.', 'pos'),
-    ("what an awesome view", 'pos'),
-    ('I do not like this restaurant', 'neg'),
-    ('I am tired of this stuff.', 'neg'),
-    ("I can't deal with this", 'neg'),
-    ('he is my sworn enemy!', 'neg'),
-    ('my boss is horrible.', 'neg')
-]
-test = [
-    ('the beer was good.', 'pos'),
-    ('I do not enjoy my job', 'neg'),
-    ("I ain't feeling dandy today.", 'neg'),
-    ("I feel amazing!", 'pos'),
-    ('Gary is a friend of mine.', 'pos'),
-    ("I can't believe I'm doing this.", 'neg')
-]
-
 cl = NaiveBayesClassifier(train)
 with open('train.json', 'r') as fp:
     cl = NaiveBayesClassifier(fp, format="json")
