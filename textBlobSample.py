dac003a32fdb672783eab848df11140065c25300
Luis Hernandez <ziul.ch@gmail.com>
2019-03-27 15:42:51 -0400

    Update textBlobSample.py

--- textBlobSample.py
+++ textBlobSample.py
@@ -1,9 +1,25 @@
 from textblob.classifiers import NaiveBayesClassifier
 
+train =[
+    ('I love this sandwich.', 'pos'),
+    ('this is an amazing place!', 'pos'),
+    ('I feel very good about these beers.', 'pos'),
+    ('this is my best work.', 'pos'),
+    ("what an awesome view", 'pos'),
+    ('I do not like this restaurant', 'neg'),
+    ('I am tired of this stuff.', 'neg'),
+    ("I can't deal with this", 'neg'),
+    ('he is my sworn enemy!', 'neg'),
+    ('my boss is horrible.', 'neg')
+]
+
+# with open('train.json', 'r') as fp:
+#     cl = NaiveBayesClassifier(fp, format="json")
+
 cl = NaiveBayesClassifier(train)
-with open('train.json', 'r') as fp:
-    cl = NaiveBayesClassifier(fp, format="json")
+
 cl.classify("This is an amazing library!")
+
 prob_dist = cl.prob_classify("This one's a doozy.")
 prob_dist.max()
 round(prob_dist.prob("pos"), 2)
