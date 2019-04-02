481091b97956f912db7ebd8c58978cdaf7b8494c
ziul.ch <ziul.ch@gmail.com>
2019-04-02 13:41:45 -0400

    Added Organized folder
    - meant for the data we''ll use

---
+++ Organized/FakeBuzzfeed/toJsonFake.py
@@ -0,0 +1,38 @@
+import os
+import re
+
+path = '.'
+
+if os.path.exists("fake.json"):
+    os.remove("fake.json")
+
+
+with open('fake.json', encoding="utf8", mode='a') as the_file:
+        the_file.write('[\n')
+        
+        for filename in os.listdir(path):
+            if filename.endswith(".txt"):
+                print (filename)
+                toRead = open(filename, 'r')
+                the_file.write("\t{ \"text\": \"")
+                
+                text = toRead.read()
+                # text = text.replace('\n', ' ')
+                # text = text.replace('\r', ' ')
+                # text = text.replace('\"', '\'')
+                # text = text.replace("\\", ' ')
+                # text = text.replace("/", ' ')
+                text = re.sub('[^a-zA-Z ]', ' ', text)
+                text = re.sub(' +', ' ', text)
+
+                the_file.write(text)
+
+                the_file.write("\", \"label\": \"Fake\"},\n")
+                toRead.close()
+                continue
+            else:
+                continue
+        the_file.write('\n]')
+
+
+
---
+++ Organized/RealPolitical/toJsonReal.py
@@ -0,0 +1,34 @@
+import os
+import re
+
+path = '.'
+
+if os.path.exists("real.json"):
+    os.remove("real.json")
+
+
+with open('real.json', encoding="utf8", mode='a') as the_file:
+        the_file.write('[\n')
+        
+        for filename in os.listdir(path):
+            if filename.endswith(".txt"):
+                print (filename)
+                toRead = open(filename, 'r')
+                the_file.write("\t{ \"text\": \"")
+                
+                text = toRead.read()
+                text = re.sub('[^a-zA-Z ]', ' ', text)
+                text = re.sub(' +', ' ', text)
+                # text = text.encode('ascii', errors='ignore')
+
+                the_file.write(text)
+
+                the_file.write("\", \"label\": \"Real\"},\n")
+                toRead.close()
+                continue
+            else:
+                continue
+        the_file.write('\n]')
+
+
+
