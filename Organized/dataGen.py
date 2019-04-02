70e701e78714259b07ef731574f38a020876b842
ziul.ch <ziul.ch@gmail.com>
2019-04-02 17:05:28 -0400

    updated dataGen.py
    - made code prettier
    - made console output more useful

--- Organized/dataGen.py
+++ Organized/dataGen.py
@@ -1,30 +1,46 @@
 import os
 import re
 
+
+"""
+Script that generates a training and testing JSON file given some directories where each data point is a single .txt file
+"""
+
+# paths to labeled data 
 pathTrainFake = '.\\FakeBuzzfeed'
 pathTrainReal = '.\\RealBuzzfeed'
 pathTestFake = '.\\FakeRandom'
 pathTestReal = '.\\RealRandom'
 
+# names for output files
 jsonTrain = "train.json"
 jsonTest = "test.json"
 
+# remove files if they already exist
 if os.path.exists(jsonTrain):
     os.remove(jsonTrain)
-
 if os.path.exists(jsonTest):
     os.remove(jsonTest)
 
+
+
 def addToJson(jsonName, pathToFolder, label, last):
+    """
+    adds multiple text files to a single json file,
+        jsonName: name of output file
+        pathToFoler: path to the folder containing the labeled data
+        label: the label of the data
+        last: boolean to indicate if this is the last folder being added (current implementation assumes you're only adding two folder to the json)
+    """
     with open(jsonName, encoding="utf8", mode='a') as the_file:
         count = 1
         nFiles = len(os.listdir(pathToFolder))
-        print("adding: " + str(nFiles) + "files")
+        print("adding: " + str(nFiles) + " files")
         if not last:
             the_file.write('[\n') 
         for doc in os.listdir(pathToFolder):
             if doc.endswith(".txt"):
-                print (str(count) + ":" +"adding: " + doc + " to " + jsonName)
+                print (str(count) + "/" + str(nFiles) +": " + doc + " to " + jsonName)
                 toRead = open(pathToFolder + "\\" + doc, 'r')
                 the_file.write("\t{ \"text\": \"")
                 text = toRead.read()
@@ -32,15 +48,11 @@ def addToJson(jsonName, pathToFolder, label, last):
                 text = re.sub(' +', ' ', text)
                 the_file.write(text)
                 if last and (count == nFiles):
-                    print ("cool")
                     the_file.write("\", \"label\": \"" + label + "\"}\n")
                 else:
                     the_file.write("\", \"label\": \"" + label + "\"},\n")
                 toRead.close()
                 count += 1
-                continue
-            else:
-                continue
         if last:
             the_file.write(']')   
     
