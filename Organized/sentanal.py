95d0dfb2b41acc4d1462b30701bb7026e2df1055
ziul.ch <ziul.ch@gmail.com>
2019-04-02 15:47:27 -0400

    Renamed textBlob to Sentanal

--- Organized/sentanal.py
+++ Organized/sentanal.py
@@ -1,26 +1,5 @@
 from textblob.classifiers import NaiveBayesClassifier
 
-
-
-print("Running!")
-
-with open('train.json', encoding='utf-8', mode='r') as train:
-    cl = NaiveBayesClassifier(train, format="json")
-
-
-print(cl.show_informative_features(5))
-
-with open('test.json', encoding='utf-8', mode='r') as test:
-    print("accuracy: " + str(cl.accuracy(test, format="json")))
-
-print("Done!!")
-
-
-
-
-# toClassify = input("classify: ")
-# print(cl.classify(toClassify))
-
 # train =[
 #     ('I love this sandwich.', 'pos'),
 #     ('this is an amazing place!', 'pos'),
@@ -35,6 +14,25 @@ print("Done!!")
 # ]
 
 # cl = NaiveBayesClassifier(train)
+
+print("Running!")
+
+with open('train.json', encoding='utf-8', mode='r') as train:
+    cl = NaiveBayesClassifier(train, format="json")
+
+
+print(cl.show_informative_features(5))
+
+with open('test.json', encoding='utf-8', mode='r') as test:
+    print("accuracy: " + str(cl.accuracy(test, format="json")))
+
+
+# toClassify = input("classify: ")
+# print(cl.classify(toClassify))
+
+
+
+print("Done!!")
 
 # prob_dist = cl.prob_classify("This one's a doozy.")
 
