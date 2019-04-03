f37a04590ab9f2219d822eda72e676ab50455cf4
mel1 <mel1@vt.edu>
2019-04-03 12:10:41 -0400

    added clean function and improved its functionality

--- Organized/dataGen.py
+++ Organized/dataGen.py
@@ -1,9 +1,13 @@
 import os
 import re
-
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem.snowball import EnglishStemmer
 
 """
-Script that generates a training and testing JSON file given some directories where each data point is a single .txt file
+Script that generates a training and testing JSON file given some
+directories where each data point is a single .txt file
 """
 
 # paths to labeled data 
@@ -21,6 +25,37 @@ if os.path.exists(jsonTrain):
     os.remove(jsonTrain)
 if os.path.exists(jsonTest):
     os.remove(jsonTest)
+
+def clean(toClean):
+    """
+    This function cleans the text before adding it to the JSON.
+    It operates by using regex to remove non-alphabetical charecters
+    and extra spcaces, then uses NLTK to remove stop words
+    """
+
+    stemmer = EnglishStemmer()
+    stopWords = set(stopwords.words('english'))
+    clean = ""
+
+    # Force lowercase
+    toClean = toClean.lower()
+
+    # remove non alphabetical chars and remove extra spaces
+    toClean = re.sub('[^a-z]', ' ', toClean) # use [^a-zA-Z] if not forced lowercase
+    # Replaces sequential spaces with single space
+    toClean = re.sub(' +', ' ', toClean)
+
+    # tokenize words for stemming and stop word removal
+    tokens = word_tokenize(toClean)
+
+    # for each token, stem token and then check if it is stop word.
+    # adds token to the clean text if it is not a stop word.
+    for token in tokens:
+        # token = stemmer.stem(token)
+        if token not in stopWords:
+            clean = clean + (str(token) + " ")
+
+    return clean
 
 
 
@@ -44,8 +79,7 @@ def addToJson(jsonName, pathToFolder, label, last):
                 toRead = open(pathToFolder + "\\" + doc, 'r')
                 the_file.write("\t{ \"text\": \"")
                 text = toRead.read()
-                text = re.sub('[^a-zA-Z ]', ' ', text)
-                text = re.sub(' +', ' ', text)
+                text = clean(text)
                 the_file.write(text)
                 if last and (count == nFiles):
                     the_file.write("\", \"label\": \"" + label + "\"}\n")
@@ -54,7 +88,7 @@ def addToJson(jsonName, pathToFolder, label, last):
                 toRead.close()
                 count += 1
         if last:
-            the_file.write(']')   
+            the_file.write(']')
     
 addToJson(jsonTrain, pathTrainFake, "neg", False)
 addToJson(jsonTrain, pathTrainReal, "pos", True)
