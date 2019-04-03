830c363f8f925e671701d3bec3094598af23337a
mel1 <mel1@vt.edu>
2019-04-03 15:04:51 -0400

    Added foundation of NLTK model script

---
+++ NLTK Sentanal/NLTK_Model.py
@@ -0,0 +1,13 @@
+import os
+import re
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem.snowball import EnglishStemmer
+
+"""
+This is a sentiment analyses model using the NLTK classifier
+
+Authors: Luis Hernandez, Jordan Jefferson, Matthew Layne
+"""
+
