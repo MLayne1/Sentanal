dda5da0ec057aaeb9a2c0b08d75bb9e13dcf1f4b
mel1 <mel1@vt.edu>
2019-04-03 18:05:11 -0400

    luis made some changes on my computer and I added the JSONs to Data/

--- NLTK Sentanal/NLTK_Model.py
+++ NLTK Sentanal/NLTK_Model.py
@@ -1,5 +1,6 @@
 import os
 import re
+import json
 import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
@@ -11,5 +12,30 @@ This is a sentiment analyses model using the NLTK classifier
 Authors: Luis Hernandez, Jordan Jefferson, Matthew Layne
 """
 
-TrainingDocs
-TestingDocs
+trainFile = open('.\\Data\\jsonFiles\\train.json') 
+testFile = open('.\\Data\\jsonFiles\\test.json') 
+
+trainArticlesJson = json.load(trainFile)
+
+trainReal = []
+trainFake = []
+
+
+for article in trainArticlesJson:
+
+	if article['label'] == 'pos':
+		trainReal.append(article['text'])
+	else:
+		trainFake.append(article['text'])
+
+print("Length of real training set: " + str(len(trainReal)))
+print(len(trainFake))
+
+
+
+
+# FakeArticlesJson = json.load()
+# RealArticlesJson = json.load()
+
+
+
