d5174936893732b6c825bc77cec256aefc879786
ziul.ch <ziul.ch@gmail.com>
2019-04-05 04:23:16 -0400

    Added JSONs
    - hFake: Horne fake data
    - hReal: Horne real data
    -  merge.py: script used to make new jsons

---
+++ NLTK Sentanal/Data/merge.py
@@ -0,0 +1,39 @@
+""" Quick script meant to merge train and test into fake and real """
+
+import json
+
+SRC_TRAIN = '.\\jsonFiles\\train.json'
+SRC_TEST = '.\\jsonFiles\\test.json'
+
+OUT_FAKE = '.\\jsonFiles\\hFake.json'
+OUT_REAL = '.\\jsonFiles\\hReal.json'
+
+
+def makeJsons(srcTest, srcTrain):
+
+    oFake = []
+    oReal = []
+
+    with open(srcTest) as jFile:
+        tj = json.load(jFile)
+        for article in tj:
+            if article['label'] == 'pos':
+                oReal.append(article)
+            else:
+                oFake.append(article)
+
+    with open(srcTrain) as jFile:
+        fj = json.load(jFile)
+        for article in fj:
+            if article['label'] == 'pos':
+                oReal.append(article)
+            else:
+                oFake.append(article)
+
+    with open(OUT_FAKE, 'w') as jsonFile:
+        json.dump(oFake, jsonFile, indent=4)
+
+    with open(OUT_REAL, 'w') as jsonFile:
+        json.dump(oReal, jsonFile, indent=4)
+
+makeJsons(SRC_TEST, SRC_TRAIN)
